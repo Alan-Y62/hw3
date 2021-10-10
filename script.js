64b751acbc07f874d586e0d4845a78d926e012e7
Alan Yuan <alan.yuan62@myhunter.cuny.edu>
2021-10-10 17:47:03 -0400

    moved script from html file back to script file

--- script.js
+++ script.js
@@ -4,11 +4,45 @@ let colorSelected;
 
 //Add a row
 function addR() {
-    alert("Clicked Add Row")
+    //alert("Clicked Add Row")
+    let table = document.querySelector('table')
+    let newRow = document.createElement('tr')
+    let newBox = document.createElement('td')
+    if(numRows !== 0) {
+        for(let i = 0; i< numCols; i++) {
+            let newBox = document.createElement('td')
+            newRow.appendChild(newBox)
+        }
+        table.appendChild(newRow)
+        numRows++;
+    }
+    else {
+        newRow.appendChild(newBox)
+        table.appendChild(newRow)
+        numCols++;
+        numRows++;
+    }
 }
 //Add a column
 function addC() {
-    alert("Clicked Add Col")
+    //alert("Clicked Add Col")
+    let table = document.querySelector('table')
+    let newCol = document.createElement('tr')
+    let newBox = document.createElement('td')
+    if(numCols !== 0) {
+        tableRow = document.querySelectorAll('tr')
+        for (i = 0; i < numRows; i++) {
+            let newBox = document.createElement('td')
+            tableRow[i].appendChild(newBox)
+        }
+        numCols++;
+    }
+    else {
+        newCol.appendChild(newBox)
+        table.appendChild(newCol)
+        numCols++;
+        numRows++;
+    }
 }
 
 //Remove a row
